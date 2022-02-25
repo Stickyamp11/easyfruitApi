9ff9f0349e00c4c3b279278883dfd407338133f0
Manu <i82cosem@uco.es>
2022-02-25 09:05:14 +0100

    Updated the general structure of the API in order to improve knowdledge from user

--- models/customerDTO.py
+++
@@ -1,5 +0,0 @@
-class customerDTO:
-    def __init__(self, name: str, location: Location, description: str = "") -> None:
-        self.id = id
-        self.location = location
-        self.description = description
